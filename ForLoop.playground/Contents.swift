093bc474230a875d2c4170519f509fa24b43ba08
MasterUNG <phrombutr@gmail.com>
2018-03-10 13:35:29 +0700

    forLoop

---
+++ ForLoop.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//For Loop Statement
+var numInt = [11,22,33,44,55]
+
+for intArray in numInt {
+    print("intArray ==> \(intArray)")
+}
+
+
+
