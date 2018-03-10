e490b675469c7c88cf89b83bfc27c23df26a8aa7
MasterUNG <phrombutr@gmail.com>
2018-03-10 09:12:27 +0700

    Variable

---
+++ VariableAndConstant.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//การประกาศตัวแปร แบบ แก้ไขค่าได้
+var nameString = "Master UNG"
+
+//ลองแก้ไขค่าของตัวแปร nameString
+nameString = "Doramon" // สามารถแก้ไขค่าได้ ถ้าเป็น var
+
+
