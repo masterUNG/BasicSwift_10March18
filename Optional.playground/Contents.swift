db5ba3b89660150c4605026b5b66e14ee68614a1
MasterUNG <phrombutr@gmail.com>
2018-03-10 14:44:43 +0700

    optional

---
+++ Optional.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Optional Story
+
+
+
+
+var numberString: String = "four"
+
+
+var numberInt = Int(numberString)
+
+
+//myNumberInt = 50
+
+if let testNumberInt = numberInt {
+    let answerInt = testNumberInt * 100
+    print("Answer ==> \(answerInt)")
+} else {
+    print("Cannot Calculate String Not Number")
+}
+
+
+
+
+
