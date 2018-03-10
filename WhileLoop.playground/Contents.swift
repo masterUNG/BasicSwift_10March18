f8c8a927db7099efd25dc6eeaa624680b92a796d
MasterUNG <phrombutr@gmail.com>
2018-03-10 13:46:55 +0700

    WhileLoop

---
+++ WhileLoop.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//While Loop Statement
+let constantInt: Int = 5
+var factorInt = 1
+
+while factorInt <= 12 {
+    
+    print("\(constantInt) x \(factorInt) = \(constantInt * factorInt)")
+    factorInt += 1
+    
+}
+
+
+
