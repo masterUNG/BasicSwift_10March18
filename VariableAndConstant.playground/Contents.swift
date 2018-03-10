a24c934c16ed2631b20d802b12aece1c0c40470b
MasterUNG <phrombutr@gmail.com>
2018-03-10 09:23:33 +0700

    Type Data

--- VariableAndConstant.playground/Contents.swift
+++ VariableAndConstant.playground/Contents.swift
@@ -3,7 +3,19 @@
 import UIKit
 
 //การประกาศตัวแปร แบบ แก้ไขค่าได้
+//การประกาศตัวแปรแบบไม่กำหนดชนิดข้อมูล แต่จะกำหนดจาก Value
 var nameString = "Master UNG"
+var numAInt = 123
+
+
+//นี่คือการประกาศตัวแปบแบบกำหนด ชนิดของข้อมูล
+var surnameString: String = "Phrumbutr"
+var numberAInt: Int = Int(123.45)
+var numberDouble: Double = 3.1416
+var statusBool: Bool = true
+
+
+
 
 //ลองแก้ไขค่าของตัวแปร nameString
 nameString = "Doramon" // สามารถแก้ไขค่าได้ ถ้าเป็น var
